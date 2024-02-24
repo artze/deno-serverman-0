43583a74f4ef243290118eca6af7f7e6f2a338d7
artze <13703523+artze@users.noreply.github.com>
2024-02-24 15:02:57 +0800

    Update `consumer` to use next()

--- consumer.ts
+++ consumer.ts
@@ -2,32 +2,38 @@ import * as serverman from "@src/mod.ts";
 
 const app = serverman.createApp();
 
-app.use(() => {
+app.use(async (_ctx, next) => {
   console.log("General handler 1");
+  await next();
 });
-app.use(() => {
+app.use(async (_ctx, next) => {
   console.log("General handler 2");
+  await next();
 });
 
 const catRouter = serverman.createRouter();
-catRouter.use((ctx) => {
+catRouter.use(async (ctx, next) => {
   console.log("General cat router");
   ctx.res.status = 200;
+  await next();
 });
-catRouter.use("/calico", () => {
+catRouter.use("/calico", async (_ctx, next) => {
   console.log("Calico cat router");
+  await next();
 });
 catRouter.get("/calico/:id", () => {
   console.log("Calico cat :id route");
 });
 
 const dogRouter = serverman.createRouter();
-dogRouter.use((ctx) => {
+dogRouter.use(async (ctx, next) => {
   console.log("General dog router");
   ctx.res.status = 200;
+  await next();
 });
-dogRouter.use("/labrador", () => {
+dogRouter.use("/labrador", async (_ctx, next) => {
   console.log("Labrador dog router");
+  await next();
 });
 
 app.use("/cat", catRouter.handlerFn);
